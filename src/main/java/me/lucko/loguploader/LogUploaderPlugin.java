20906930770eff8a78466650bacc69cd005f5395
Luck <git@lucko.me>
2018-08-03 14:53:31 +0100

    convert from gist to bytebin

--- src/main/java/me/lucko/loguploader/LogUploaderPlugin.java
+++ src/main/java/me/lucko/loguploader/LogUploaderPlugin.java
@@ -1,18 +1,21 @@
 package me.lucko.loguploader;
 
-import com.google.common.collect.Maps;
+import com.google.common.io.ByteStreams;
+
+import me.lucko.loguploader.http.Bytebin;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collections;
+import java.util.zip.GZIPOutputStream;
 
 public class LogUploaderPlugin extends JavaPlugin {
 
@@ -27,8 +30,12 @@ public class LogUploaderPlugin extends JavaPlugin {
             }
 
             try {
-                String content = new String(Files.readAllBytes(logFile), StandardCharsets.UTF_8);
-                String url = PasteUtils.paste("latest.log", Collections.singletonList(Maps.immutableEntry("latest.log", content)));
+                ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+                try (InputStream in = Files.newInputStream(logFile); GZIPOutputStream out = new GZIPOutputStream(byteOut)) {
+                    ByteStreams.copy(in, out);
+                }
+
+                String url = Bytebin.postCompressedContent(byteOut.toByteArray());
                 sender.sendMessage(ChatColor.RED + "Upload complete. URL: " + url);
             } catch (IOException e) {
                 e.printStackTrace();
